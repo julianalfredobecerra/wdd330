853835c6c947e1a26378a5f8d8af2d17d04be7a5
julianbrom1 <bec21001@byui.edu>
2025-07-14 20:26:40 -0500

    changes

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,7 +1,13 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = getLocalStorage("so-cart") || [];
+
+  if (cartItems.length === 0) {
+    document.querySelector(".product-list").innerHTML = `<li class="cart-empty>Your cart is empty.</li>`;
+    return;
+  }
+
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
