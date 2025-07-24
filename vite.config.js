8acd85749d3a714b4bebf4e51fffdf663ccb365b
julianbrom1 <bec21001@byui.edu>
2025-07-23 22:09:13 -0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -8,10 +7,11 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
-        cart: resolve(__dirname, "src/cart/index.html"),
-        checkout: resolve(__dirname, "src/checkout/index.html"),
-        product: resolve(__dirname, "src/product_pages/index.html"),
+        main: "/index.html",
+        cart: "cart/index.html",
+        checkout: "checkout/index.html",
+        product: "product_pages/index.html",
+        listing: "product_listing/index.html",
       },
     },
   },
